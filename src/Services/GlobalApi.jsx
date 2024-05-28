c8ba983b383920d3c583754b766ae3f8d401ed91
Watcharapat <earth.bootcamp@gmail.com>
2024-05-28 15:48:20 +0700

    Final

---
+++ src/Services/GlobalApi.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const movieBaseUrl="https://api.themoviedb.org/3"
+const api_key='0d49795a83cb291e75a1a05a2f8a571e'
+
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=0d49795a83cb291e75a1a05a2f8a571e'
+
+//https://api.themoviedb.org/3/trending/all/day?api_key=0d49795a83cb291e75a1a05a2f8a571e
+const getTrendingVideos=axios.get(movieBaseUrl+
+    "/trending/all/day?api_key="+api_key);
+    const getMovieByGenreId=(id)=>
+    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+
+export default {
+    getTrendingVideos,
+    getMovieByGenreId
+}
